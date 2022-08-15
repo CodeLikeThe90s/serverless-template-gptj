a11761c0147c12b0dc3e1d738d4b80f457375384
Nelson Chu <csnelsonchu@gmail.com>
2022-08-15 14:10:51 +0800

    use pipeline

--- app.py
+++ app.py
@@ -1,48 +1,33 @@
-from transformers import GPTJForCausalLM, GPT2Tokenizer
+from transformers import pipeline
 import torch
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-
 # Init is ran on server startup
-# Load your model to GPU as a global variable here.
+# Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    global tokenizer
-
-    print("loading to CPU...")
-    model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True)
-    print("done")
-
-    # conditionally load to GPU
-    if device == "cuda:0":
-        print("loading to GPU...")
-        model.cuda()
-        print("done")
-
-    tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-j-6B")
-
+    
+    device = 0 if torch.cuda.is_available() else -1
+    print("Active Device:{}".format(device))
+    model = pipeline('text-generation', model='EleutherAI/gpt-j-6B', device=device)
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
-    global tokenizer
 
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
+    max_length = model_inputs.get('max_length', None)
+    min_length = model_inputs.get('min_length', None)
     if prompt == None:
         return {'message': "No prompt provided"}
+    if max_length == None:
+        return {'message': "No max_length provided"}
+    if min_length == None:
+        return {'message': "No min_length provided"}
     
-    # Tokenize inputs
-    input_tokens = tokenizer.encode(prompt, return_tensors="pt").to(device)
-
     # Run the model
-    output = model.generate(input_tokens)
-
-    # Decode output tokens
-    output_text = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
-
-    result = {"output": output_text}
+    result = model(prompt, max_length=max_length, min_length=min_length, do_sample=False)[0]
 
     # Return the results as a dictionary
     return result
